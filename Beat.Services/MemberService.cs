7d50f1c8dd4f930eae1c425c1597069772596e42
ahmad magdy <ahmad.magdy@nebrastech.com>
2020-09-28 21:12:44 +0200

    add beat services project

---
+++ Beat.Services/MemberService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Beat.Data;
+using Beat.Model;
+using Beat.Interfaces;
+
+namespace Beat.Services
+{
+    public class MemberService: IMemberService
+    {
+        private BeatContext db;
+
+        public MemberService()
+        {
+            db = new BeatContext();
+        }
+
+        public List<Member> GetAll()
+        {
+            return db.Members.ToList();
+        }
+
+        public Member GetById(Guid id)
+        {
+            return db.Members.FirstOrDefault(m => m.Id == id);
+        }
+    }
+}
