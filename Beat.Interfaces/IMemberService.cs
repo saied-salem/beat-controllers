58277b38146641025202b6179c300246303d1f8f
ahmad magdy <ahmad.magdy@nebrastech.com>
2020-09-28 21:13:41 +0200

    add interfaces

---
+++ Beat.Interfaces/IMemberService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Beat.Model;
+
+namespace Beat.Interfaces
+{
+    public interface IMemberService
+    {
+        List<Member> GetAll();
+        Member GetById(Guid id);
+    }
+}
