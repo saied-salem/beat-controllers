f5b99e0fa3edfad5e8bde480feb9f481a11bbf26
ahmad magdy <ahmad.magdy@nebrastech.com>
2020-09-28 21:13:58 +0200

    finalize first phase for backend

--- Beat/Controllers/HomeController.cs
+++ Beat/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Beat.Interfaces;
 using Beat.Model;
+using Beat.Services;
 
 namespace Beat.Controllers
 {
     public class HomeController : Controller
     {
+        private IMemberService _memberService;
+
+        public HomeController()
+        {
+            _memberService = new MemberService();
+        }
         public ActionResult Index()
         {
+            var member = _memberService.GetAll();
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
